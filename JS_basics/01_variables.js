1a7ceae504286e69d487d097dad5bb845163717b
Sumitchandra478 <105159137+Sumitchandra478@users.noreply.github.com>
2025-01-03 18:48:32 +0000

    datatypes

---
+++ JS_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=173660
+
+let email="sumitchandra478@gmail.com";
+var accountPassword="Sumit@89799"
+accountCity="Pune"
+// console.log(accountId);
+
+// // accountId=1735;
+// // console.log(accountId);
+
+// // Const cant be changed
+
+/*
+Avoid using Var because of issue in block scope and function scope
+
+*/
+
+
+console.table([email,accountPassword,accountCity,accountId]);
---
+++ JS_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let age="33abc";
+
+// let valueinNumber=Number(age);
+// console.log(typeof age);
+// console.log(valueinNumber)  //returns NaN
+// console.log(typeof valueinNumber) //type converts to number only
+
+// age=null;
+// valueinNumber=Number(age);
+// console.log(valueinNumber);  //returns 0
+
+// age=undefined;
+// valueinNumber=Number(age);
+// console.log(valueinNumber);  //returns NaN
+
+// age=true;
+// valueinNumber=Number(age);
+// console.log(valueinNumber); //returns 1;
+
+
+// let num1=1
+// let getBoolean=Boolean(num1)
+// console.log(getBoolean)  //true
+
+// let num1=null
+// let getBoolean=Boolean(num1)
+// console.log(getBoolean)
+
+let str=undefined;
+str2=String(str);
+console.log(str2);
