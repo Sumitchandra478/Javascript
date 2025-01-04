c58645eb87ea18fddb8fae752ebd314cda864a4e
Sumitchandra478 <105159137+Sumitchandra478@users.noreply.github.com>
2025-01-04 19:28:22 +0000

    date

---
+++ JS_basics/07_datesandtime.js
@@ -0,0 +1,28 @@
+let myDate=new Date();
+// console.log(myDate.toString());  //Sat Jan 04 2025 18:43:29 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toLocaleString());  //1/4/2025, 6:43:29 PM
+// console.log(myDate.toISOString());  //2025-01-04T18:43:29.785Z
+// console.log(myDate.toLocaleDateString());  //1/4/2025
+// console.log(myDate.toDateString())  //Sat Jan 04 2025
+
+// //Interview typeOf Date ==>object
+
+// console.log(typeOf(myDate) );
+
+// let myDate=new Date(2024,0,5,3,5,56)
+// console.log(myDate)
+// console.log(myDate.toLocaleString())
+
+// let timeStamp=Date.now();
+// console.log(timeStamp)
+// console.log(myDate.getTime());
+// console.log(myDate.getDay())  //if returns 6 then last day of week means Sun
+
+myDate.toLocaleString('default',{
+    weekday:"long"
+})
+console.log(myDate.toLocaleString())
+
+
+
+
