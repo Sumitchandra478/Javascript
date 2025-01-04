c17b34928741e253776cd0e0c231a604fca356cd
Sumitchandra478 <105159137+Sumitchandra478@users.noreply.github.com>
2025-01-04 17:32:31 +0000

    sumbit

---
+++ JS_basics/05_strings.js
@@ -0,0 +1,33 @@
+// let name="Sumit"
+
+// let age =25
+
+// console.log(`My name is ${name} & my age is ${age}`)
+
+// // Another way of defining strings but its not array but key valye pair,where value can be obtained using 
+
+// const name2=new String('Sumit Chandra')
+
+// // console.log(name2.toUpperCase)
+
+// // here name2 is not changed to upper case as its stack
+
+// // Methods in Strings
+
+// const anotherName=name2.slice(0,4)  //works as substring from 0 to 3
+
+// // console.log(anotherName)
+
+// const anotherName1=name2.slice(3)   //starting 3 uda do
+// console.log(anotherName1)
+
+// const name3=name2.slice(-13,4)  //negative means from last indexing position ,-13 is length so o/p Sumi
+// console.log(name3);
+
+//Split
+
+
+const myName="Sumit_chandra478"
+
+console.log(myName.split('_'))
+
