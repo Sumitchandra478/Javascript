854b1eae47075297b36b76908feaa959bac402cd
Sumitchandra478 <105159137+Sumitchandra478@users.noreply.github.com>
2025-01-04 18:02:58 +0000

    save

---
+++ JS_basics/06_numbers&maths.js
@@ -0,0 +1,20 @@
+const score=new Number(278)
+
+//toString gets converted to string and then all string methods can be applied
+
+
+
+// console.log(score.toFixed(3))  //toFixed  upto that provided number add decimals
+
+// let num=123.456
+
+// console.log(num.toPrecision(3));
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+const min=10;
+const max=40;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
