9ec119930ad175dec648c9a92153aa0a9661bfa6
Sumitchandra478 <105159137+Sumitchandra478@users.noreply.github.com>
2025-01-06 14:22:25 +0000

    sav

--- JS_basics02/04_objects.js
+++ JS_basics02/04_objects.js
@@ -1,20 +1,46 @@
 //AS consructor
 
-const tinderUser=new Object()
+// const tinderUser=new Object()  //singleton
 
-const myUser={
-    fullDetails:{
-       email:"sumitchandra478@gmail.com",
-       fullName:{
-        firstName:"Sumit",
-        lastName:"Chandra"
-       }
-    }
-}
-console.log(myUser.fullDetails) /*{
-    email: 'sumitchandra478@gmail.com',
-    fullName: { firstName: 'Sumit', lastName: 'Chandra' }
-  } */
-console.log(myUser.fullDetails.fullName)
+// const myUser={
+//     fullDetails:{
+//        email:"sumitchandra478@gmail.com",
+//        fullName:{
+//         firstName:"Sumit",
+//         lastName:"Chandra"
+//        }
+//     }
+// }
+// console.log(myUser.fullDetails) /*{
+//     email: 'sumitchandra478@gmail.com',
+//     fullName: { firstName: 'Sumit', lastName: 'Chandra' }
+//   } */
+// console.log(myUser.fullDetails.fullName)
 
 //{ firstName: 'Sumit', lastName: 'Chandra' }
+
+const myElem1={1:'a',2:'b'}
+const myElem2={3:'c',4:'d'}
+const myObj1={myElem1,myElem2}
+// console.log(myObj1)  //{ myElem1: { '1': 'a', '2': 'b' }, myElem2: { '3': 'c', '4': 'd' } } lie array
+const Obj3=Object.assign(myElem1,myElem2)
+// console.log(Obj3)
+// console.log(Object.keys(Obj3))
+console.log(Object.entries(Obj3))  //[ [ '1', 'a' ], [ '2', 'b' ], [ '3', 'c' ], [ '4', 'd' ] ]
+
+// const obj3={...myElem1,...myElem2}
+// console.log(obj3)
+
+// in terms of array
+
+// const myObj3=[
+//   {},
+//   {1:'a',2:'b'},
+//   {2:"SUmit",3:"Chandra"},
+//   {}
+// ]
+// //myObj3[0] ==>is an object
+
+// console.log(Object.keys(myElem1))
+// console.log(Object.values(myElem2))
+
---
+++ JS_basics02/05_objects.js
@@ -0,0 +1,9 @@
+const course={
+    courseName:"javascript from Chai aur Code",
+    price:999,
+    instructor:"Hitesh"
+}
+
+console.log(course.instructor)
+const {instructor}=course;
+console.log(instructor)
