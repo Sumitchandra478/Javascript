6906b3f327b97f12e0477c3ee411898cf0372bc0
Sumitchandra478 <105159137+Sumitchandra478@users.noreply.github.com>
2025-01-07 18:34:55 +0000

    save

--- JS_basics/02_dataTypes.js
+++ JS_basics/02_dataTypes.js
@@ -1,2 +1,32 @@
-console.log(typeof null);   //object
-console.log(typeof undefined) //undefined
+// console.log(typeof null);   //object
+// console.log(typeof undefined) //undefined
+
+// let a=10;
+// let b=a++;
+// console.log(b)  //10 only and not 11
+// let c=null
+// console.log(+c)
+
+// if(false){
+//     console.log("this is false")
+// }
+// else{
+//     console.log("true")
+// }
+
+// let num5=1000000
+// console.log(num5.toLocaleString('en-IN'))
+
+// let myDate=new Date(2024,3,15,3,56,10)
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.getMonth())
+
+const myArr=[2,4,6,8,10]
+const newArr=myArr
+myArr[2]=19
+console.log(myArr)
+console.log(newArr)
