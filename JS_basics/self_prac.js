c27b233328238a38d6d7c6a609cd220eb5929375
Sumitchandra478 <105159137+Sumitchandra478@users.noreply.github.com>
2025-01-07 20:10:21 +0000

    save

---
+++ JS_basics/self_prac.js
@@ -0,0 +1,74 @@
+// let a=10;
+// let b=a++;
+// console.log(b)  //10 only and not 11
+// let c=null
+// console.log(+c)
+
+// if(false){
+//     console.log("this is false")
+// }
+// else{
+//     console.log("true")
+// }
+
+// let num5=1000000
+// console.log(num5.toLocaleString('en-IN'))
+
+// let myDate=new Date(2024,3,15,3,56,10)
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.getMonth())
+
+// const myArr=[2,4,6,8,10]
+// const newArr=myArr
+// myArr[2]=19
+// console.log(myArr)
+// console.log(newArr)
+
+// let a=12
+
+// let b=a++
+// console.log(a)
+// let c=b
+// console.log(b)
+// console.log(c)
+
+
+// let name="Sumit_Chandra"
+// // console.log(name.slice(-13,4))
+// // let name2=name.slice(-12)
+// // console.log(name2)
+
+// console.log(name.slice(4,-3))   //t_chan
+// console.log(name.slice(-3,4))   // no op 
+// console.log(name.slice(-8,-4))   
+
+// const arr=[2,4,6,7,8]
+// const arr2=[1,3,5,9,11]
+
+// const arr3=arr.concat(arr2)
+// console.log(arr3)
+// const arr6=[...arr,...arr2]
+// console.log(arr6)
+// console.log(...arr,...arr2)
+// console.log(typeof arr6)
+
+// const arr3=[...arr,...arr2]
+// const arr4=[arr.join(arr2)]
+
+// console.log(arr.join());
+// console.log(arr.join(arr2));
+
+// console.log(arr3);
+// console.log(Math.max(...arr3));
+
+// console.log(arr[12])
+
+
+// let myName="Dev"
+// console.log(myName.charAt(5))
+
+console.log(Array.from(({name:"Sumit"})))
