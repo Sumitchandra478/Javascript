5cc06a5aba33812c00ebaf8917775fcd730bd4bf
Sumitchandra478 <105159137+Sumitchandra478@users.noreply.github.com>
2025-01-10 15:31:04 +0000

    s

--- JS_basics02/03_objects.js
+++ JS_basics02/03_objects.js
@@ -1,33 +1,54 @@
-const mySym=Symbol("key1");
+//  const mySym=Symbol("key1");
 
-const myObj={
-    name:"Sumit",
-    email:"sumitchandra478@gmail.com",
-    age:18,
-    isLoggedIn:true,
-    //define symbol as object
-    [mySym]:"myKey1"   //though mySym has stored symbol earlier but here its not symbol but string
-}
+// const myObj={
+//     name:"Sumit",
+//     email:"sumitchandra478@gmail.com",
+//     age:18,
+//     isLoggedIn:true,
+//     //define symbol as object
+//     [mySym]:"myKey1"   //though mySym has stored symbol earlier but here its not symbol but string
+// }
 
-// console.log(myObj.email) //or
-// console.log(myObj["email"])
-// console.log(typeof myObj[mySym])   //not in " " then its defined as symbol
+// // // console.log(myObj.email) //or
+// // // console.log(myObj["email"])
+// // // console.log(typeof myObj[mySym])   //not in " " then its defined as symbol
 
 // myObj.email="dev478@gmail.com"
 // console.log(myObj)  //obj is call by ref and non primitive so value in ref will be changed
 
-// Object.freeze(myObj)
-// myObj.name="Dev"
-// console.log(myObj["name"])   //o/p is sumit only
+// // Object.freeze(myObj)
+// // myObj.name="Dev"
+// // console.log(myObj["name"])   //o/p is sumit only
 
-myObj.greeting=function(){
-    console.log("Hello JS user")
+// myObj.greeting=function(){
+//     console.log("Hello JS user")
+// }
+
+// // console.log(myObj.greeting())
+// //console.log(`${myObj.greeting()}+${myObj["name"]}`)
+
+// myObj.greeting2=function(){
+//     console.log(`Hello JS User, ${this.name}`)
+// }
+// console.log(myObj.greeting2())  //gives undefined
+
+const myObj={
+    name:"Sumit",
+    mobile:8112206447,
+    Address:"Pune"
 }
+myObj.name="Chandra"
+// const {name}=myObj
+// console.log(name)
+// console.log(myObj)
+console.log(Object.values(myObj))
 
-// console.log(myObj.greeting())
-//console.log(`${myObj.greeting()}+${myObj["name"]}`)
+console.log(myObj.name)
+const {name}=myObj
+console.log(`${name}`)
 
-myObj.greeting2=function(){
-    console.log(`Hello JS User, ${this.name}`)
+console.log(myObj["name"])
+
+for(const key in myObj){
+    console.log(myObj[key])
 }
-console.log(myObj.greeting2())  //gives undefined
